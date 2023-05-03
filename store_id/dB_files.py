4ed94e14fe5ffd0d0201e9c817c8448bbc767427
WIA <131862530+Joeltchoupe@users.noreply.github.com>
2023-05-03 12:45:41 +0100

    Create dB_files.py

---
+++ store_id/dB_files.py
@@ -0,0 +1,1 @@
+#this code aims to create a blockchain-based database that's used to store encrypted identification datas and to offer to users functionnalities to access those datas by a decentrailsed way.
