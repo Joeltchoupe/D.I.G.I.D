256a991b2825242bbc8947bbf96e509c8e3326cf
WIA <131862530+Joeltchoupe@users.noreply.github.com>
2023-12-16 09:20:15 +0100

    Create clientable.sql

---
+++ CONTACTS_MANAGEMENT/databases/clientable.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Client (
+    ClientID SERIAL PRIMARY KEY,
+    Nom VARCHAR(50) NOT NULL,
+    Prenom VARCHAR(50) NOT NULL,
+    DateNaissance DATE,
+    Sexe CHAR(1),
+    AdressePostale VARCHAR(255),
+    AdresseEmail VARCHAR(100),
+    NumeroTelephone VARCHAR(20),
+    NumeroSecuriteSociale VARCHAR(20),
+    EtatCivil VARCHAR(20),
+    SituationFamiliale VARCHAR(50),
+    Nationalite VARCHAR(50),
+    Profession VARCHAR(50),
+    Revenu DECIMAL(15, 2),
+    SituationPatrimoniale TEXT
+);
